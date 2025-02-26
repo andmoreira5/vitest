66e841f8afc19a432b4db699f303feb6d0e487d7
Anderson <32337563+andmoreira5@users.noreply.github.com>
2025-02-26 11:38:20 -0300

    badge component test

--- src/components/Badge/Badge.test.jsx
+++ src/components/Badge/Badge.test.jsx
@@ -6,14 +6,42 @@ import { FaCheckCircle } from "react-icons/fa";
 
 describe("Badge", () => {
   let badgeTestId;
+  let iconTestId;
+  let newRender;
+  // Setup the test before each test case
   beforeEach(() => {
-    render(<Badge text="Aprovado" color="green" icon={FaCheckCircle} />);
+    const { rerender } = render(
+      <Badge text="Aprovado" color="green" icon={FaCheckCircle} size="sm" />
+    );
     badgeTestId = screen.getByTestId("badge");
+    iconTestId = screen.getByTestId("badge-icon");
+    newRender = rerender;
   });
 
   test("Should be able to see the component on the screen", () => {
     expect(badgeTestId).toBeInTheDocument();
   });
 
-  // test('Should be able to see ')
+  test("Should render the Badge with the correct text", () => {
+    expect(badgeTestId).toHaveTextContent("Aprovado");
+  });
+
+  test("Ensure that the color and the text have been applied correctly", () => {
+    expect(badgeTestId).toHaveClass(
+      "bg-green-200 text-green-800 text-xs px-2 py-1"
+    );
+  });
+
+  test("Should render with the icon when the icon prop is provided", () => {
+    expect(iconTestId).toBeInTheDocument();
+  });
+
+  // New render for the test with no icon
+  test("Should not render the icon when the icon prop is not provided", () => {
+    // Rerender the Badge without the icon prop.
+    //I need to use newRender when I want to render the component with other properties.
+    newRender(<Badge text="Aprovado" color="green" size="sm" />);
+    // Expect the icon to be null
+    expect(screen.queryByTestId("badge-icon")).toBeNull();
+  });
 });
